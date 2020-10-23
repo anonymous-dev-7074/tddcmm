bd0cecbc2b8ca26b4878763d5e520e6470d59c07
Glitch (foul-lavender-meteorology) <none>
2020-10-23 09:55:46 +0000

    👮💃 Checkpoint ./commands/level.js:23941564/1123

--- commands/level.js
+++ commands/level.js
@@ -30,7 +30,39 @@ exports.run = (client, msg, args) => {
     .setRank(rank)
     .setLevel(level)
     .setCurrentXP(exp)
-    .setRequiredXP(neededXP)
+    .const { MessageEmbed } = requir("discord.js");
+
+const { MessageAttachment } = require("discord.js");
+const canvacord = require("canvacord");
+
+exports.run = (client, msg, args) => {
+  const member =
+    msg.mentions.users.first() ||
+    msg.guild.members.cache.find(
+      mem => mem.user.username.toLowerCase() === args.join(" ").toLowerCase()
+    ) ||
+    msg.guild.members.cache.get(args[0]);
+  let user =
+    msg.mentions.users.first() ||
+    msg.guild.members.cache.find(
+      mem => mem.user.username.toLowerCase() === args.join(" ").toLowerCase()
+    ) ||
+    msg.guild.members.cache.get(args[0]);
+  if (!user) user = msg.author;
+  const xpForLevel = level => Math.ceil(level * level * 100);
+  const calcLevel = xp => Math.floor(0.1 * Math.sqrt(xp));
+  const curLevel = calcLevel(
+    client.profile.get(`${msg.guild.id}-${user.id}`, "levelpoints")
+  ); // 2
+  const pointsNeeded = xpForLevel(curLevel + 1);
+
+  const card = new canvacord.Rank()
+    .setUsername(member.username)
+    .setDiscriminator(member.discriminator)
+    .setRank(rank)
+    .setLevel(level)
+    .setCurrentXP(exp)
+    .setRequiredXP(neededXP)setRequiredXP(neededXP)
     .setStatus(user.presence.status)
     .setAvatar(user.displayAvatarURL({ format: "png", size: 1024 }));
 
