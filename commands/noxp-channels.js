34db0891135cb51bc6ef23d77dfe75859a29d5bc
Glitch (foul-lavender-meteorology) <none>
2020-10-24 06:32:26 +0000

    🍠⛪️ Checkpoint ./commands/noxp-channels.js:23941564/6942

---
+++ commands/noxp-channels.js
@@ -0,0 +1,26 @@
+let { MessageEmbed } = require("discord.js");
+const discord = require("discord.js");
+exports.run = (client, msg, args) => {
+  if (!msg.member.hasPermission("ADMINISTRATOR"))
+    return msg.channel
+      .send("You do not have the required permission to use this command.")
+      .then(m => {
+        setTimeout(() => {
+          m.delete();
+        }, 3000);
+      });
+
+  let channels = client.settings.get(msg.guild.id, channel.id, "noxpchannels");
+
+  let embed = new discord.MessageEmbed()
+    .setTitle("THESE ARE THE CHANNELS WHICH WERE SETTED FOR NO XP GAIN")
+    .setDescription(channels)
+    .setColor("#000000");
+
+  msg.channel.send(embed);
+};
+
+module.exports.help = {
+  name: "noxpchannels",
+  usage: "!set-noxpchannel <channel> | !set-noxpchannel #channel"
+};
