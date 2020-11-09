a08644ca345a73a8d647f2d0438db6a5465bf679
Glitch (tddc-leveling) <none>
2020-11-09 06:07:47 +0000

    🙎🏋 Checkpoint ./commands/level.js:15319430/1890 ./package.json:15319430/1186

--- commands/level.js
+++ commands/level.js
@@ -1,20 +1,47 @@
 const { MessageEmbed } = require('discord.js')
+const canvacord = require("canvacord")
+const status1 = {
+    online: "online",
+    idle: "idle",
+    dnd: "dnd",
+    offline: "offline"
+};
+const status2 = {
+    online: "#00ffff",
+    idle: "YELLOW",
+    dnd: "RED",
+    offline: "GREY"
+};
+const discord = require("discord.js")
 
 exports.run = (client, msg, args) => {
 	let user = msg.mentions.users.first() || msg.guild.members.cache.find(mem => mem.user.username.toLowerCase() === args.join(" ").toLowerCase()) || msg.guild.members.cache.get(args[0])
 	if (!user) user = msg.author
+  let hh = user.username
+      let bb = user.discriminator
+    const Level = client.profile.get(`${msg.guild.id}-${user.id}`, "level")
+    const XP = client.profile.get(`${msg.guild.id}-${user.id}`, "levelpoints")
     const xpForLevel = level => Math.ceil(level*level*100);
     const calcLevel = xp => Math.floor(0.1*Math.sqrt(xp));
     const curLevel = calcLevel(client.profile.get(`${msg.guild.id}-${user.id}`, "levelpoints")) // 2
     const pointsNeeded = xpForLevel(curLevel + 1);
-    let embed = new MessageEmbed()
-    .setAuthor(user.tag, user.displayAvatarURL)
-    .setColor("RANDOM")
-    .setDescription(`Level: **` + client.profile.get(`${msg.guild.id}-${user.id}`, "level") + '**' + '\n' + `XP: ${client.profile.get(`${msg.guild.id}-${user.id}`, "levelpoints")}/${pointsNeeded} (${pointsNeeded - client.profile.get(`${msg.guild.id}-${user.id}`, "levelpoints")} needed)`)
-    
-    msg.channel.send(embed)
-
-}
+    let uLevel = Level + 1;
+   const rank = new canvacord.Rank()
+            .setAvatar(user.displayAvatarURL({ dynamic: true })) 
+            .setCurrentXP(XP)
+            .setRequiredXP(pointsNeeded)
+            .setStatus(status1[user.presence.status])
+            .setProgressBar(status2[user.presence.status])
+            .setUsername(hh)
+            .setDiscriminator(bb)
+            .setLevel(Level)
+            .setRank(uLevel)
+        rank.build()
+    .then(data => {
+        const attachment = new discord.MessageAttachment(data);
+        msg.channel.send(attachment);
+    });
+    }
 
 module.exports.help = {
     name:"level",
