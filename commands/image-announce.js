8dd45b0e51b2dd7c3613d156a7d5c83b58a2c725
anonymous-dev-7074 <73049664+anonymous-dev-7074@users.noreply.github.com>
2020-11-10 13:35:29 +0530

    Create image-announce.js

---
+++ commands/image-announce.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "image-announce",
+  aliases: ["iannounce"],
+  run: async (client, message, args) => {
+    const chm = message.mentions.channels.first();
+    if (!chm) return message.reply("First mention A Channel Then Image");
+
+    const image = message.attachments.first()
+      ? message.attachments.first().proxyURL
+      : null || args[1];
+    if (!image) return message.reply("Provide A Image");
+
+    const embed = new Discord.MessageEmbed()
+      .setImage(image, { dynamite: 1024 })
+      .setColor("FF7034");
+
+    chm.send(embed);
+  }
+};
