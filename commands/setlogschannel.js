2e4b7dba2edc41daecee43619e426a83b294b979
anonymous-dev-7074 <73049664+anonymous-dev-7074@users.noreply.github.com>
2020-11-14 13:14:54 +0530

    Create setlogschannel.js

---
+++ commands/setlogschannel.js
@@ -0,0 +1,38 @@
+let { MessageEmbed } = require('discord.js')
+const db = require("quick.db")
+exports.run = (client, msg, args) => {
+  
+if (!msg.member.hasPermission("ADMINISTRATOR")) {
+      const embed2 = new MessageEmbed()
+        .setColor("RED")
+        .setDescription(
+          "**You Dont Have Permission To User This Command. Required Permission** `ADMINISTRATOR`"
+        );
+      return msg.channel.send(embed2);
+    }
+  let channel = msg.mentions.channels.first();
+  if (!channel) {
+        const emb = new MessageEmbed()
+          .setColor("RED")
+          .setTitle("<:Cross:744161251743105035> Channel Missing")
+          .setDescription("Please Mention A Channel To Set As logs Channel!")
+          .setTimestamp();
+        return msg.channel.send(emb);} else {
+  
+db.set(`logschannel_${msg.guild.id}`, channel.id);
+const emb = new MessageEmbed()
+          .setColor("#00FF00")
+          .setDescription(`Welcome Channel Seted As ${channel}`)
+          .setTitle("<:yesk:744161409016922123> loge Channel Seted")
+          .setTimestamp();
+        msg.channel.send(emb);
+        }
+}
+  
+  
+  
+  
+  module.exports.help = {
+    name:"setlogschannel",
+    usage: '+setlogschannel #channel',
+  }
