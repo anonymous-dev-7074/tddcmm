c4c3bda605b2f59354335d3eecb8b3612e8c92e2
anonymous-dev-7074 <73049664+anonymous-dev-7074@users.noreply.github.com>
2020-11-14 13:22:31 +0530

    Update logs.js

--- logs.js
+++ logs.js
@@ -6,6 +6,8 @@ client.once('ready', () => {
 });
 
 client.on('messageDelete', message => {
+let logschannel = db.get(`logschannel_${member.guild.id}`);
+if (!logs) return;
 	console.log(`A message by ${message.author.tag} was deleted, but we don't know by who yet.`);
 });
 
