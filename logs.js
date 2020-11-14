14e5acdb072348346a69c4a666117771a38ff2f8
anonymous-dev-7074 <73049664+anonymous-dev-7074@users.noreply.github.com>
2020-11-14 13:50:46 +0530

    Update logs.js

--- logs.js
+++ logs.js
@@ -7,14 +7,6 @@ client.once('ready', () => {
 });
 
 client.on('messageDelete', message => {
-let logschannel = db.get(`logschannel_${member.guild.id}`);
-
-
-const logsembed = new MessageEmbed()
-.setTitle("NEW LOG DETECTED")
-.setDescription(`A message by ${message.author.tag} was deleted, but we don't know by who yet.`)
-.setColor("#FF7034");
-	client.channels.cache.get(logschannel).send(logsembed)
+console.log(`A message by ${message.author.tag} was deleted, but we don't know by who yet.`);
 });
-
 client.login("NzY4NTE3MjI2NjU0ODU5Mjk1.X5BncQ.iNJHaa-q8ry-n7GUm-jWRI0Qo-k");
