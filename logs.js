0c4f658abb6354354620d30d0a2a4b0831d8ed53
anonymous-dev-7074 <73049664+anonymous-dev-7074@users.noreply.github.com>
2020-11-14 13:29:54 +0530

    Update logs.js

--- logs.js
+++ logs.js
@@ -1,14 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { MessageEmbed } = require('discord.js')
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('logs Ready!');
 });
 
 client.on('messageDelete', message => {
 let logschannel = db.get(`logschannel_${member.guild.id}`);
-if (!logs) return;
-	console.log(`A message by ${message.author.tag} was deleted, but we don't know by who yet.`);
+if (!logschannel) return;
+const logsembed = new MessageEmbed()
+.setTitle("NEW LOG DETECTED")
+.setDescription(`A message by ${message.author.tag} was deleted, but we don't know by who yet.`)
+.setColor("#FF7034");
+	client.channels.cache.get(logschannel).send(logsembed)
 });
 
-client.login('your-token-goes-here');
+client.login("NzY4NTE3MjI2NjU0ODU5Mjk1.X5BncQ.iNJHaa-q8ry-n7GUm-jWRI0Qo-k");
