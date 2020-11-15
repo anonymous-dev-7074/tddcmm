763d575ffe77fb8bd5a3a86c49fd460eb76ffa32
Raj Chatterjee <65060180+raj123567@users.noreply.github.com>
2020-11-15 14:35:11 +0530

    Update faith.js

--- faith.js
+++ faith.js
@@ -31,6 +31,16 @@ client.settings = new Enmap({ name: "settings", fetchAll: true });
 client.reactionroles = new Enmap({ name: "reactionroles", fetchAll: true });
 client.blacklisted = new Enmap({ name: "blacklisted" });
 client.applications = new Enmap({ name: "applications", fetchAll: true });
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+console.log(`Loaded ${files.length} events!`)
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
 fs.readdir("./commands/", (err, files) => {
   if (err) console.error(err);
   let jsfiles = files.filter(f => f.split(".").pop() === "js");
