aece670815bc2f75ba3485797786b6fca1bf75cf
Raj Chatterjee <65060180+raj123567@users.noreply.github.com>
2020-11-16 14:30:02 +0530

    Create stop.js

---
+++ commands/stop.js
@@ -0,0 +1,20 @@
+module.exports = {
+help: {
+name: "stop"
+},
+run: async(client, message, args) => {
+        const bot = this.client;
+        const voiceChannel = message.member.voice.channel;
+
+        const player = bot.music.players.get(message.guild.id);
+
+        if(!player) return message.channel.send("No song/s currently playing in this guild.");
+
+        if(!voiceChannel || voiceChannel.id !== player.voiceChannel.id) return message.channel.send("You need to be in a voice channel to use the leave command.");
+
+        bot.music.players.destroy(message.guild.id);
+
+        return message.channel.send("Successfully stopped the music.")
+  
+}
+}
