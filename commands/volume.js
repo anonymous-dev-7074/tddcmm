dcd047c413642751f3561805d4489dab7450a33e
Raj Chatterjee <65060180+raj123567@users.noreply.github.com>
2020-11-16 14:43:49 +0530

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -4,7 +4,7 @@ name: "volume",
 aliases: ["vol", "v"]
 },
 run: async(client, message, args) => {
-        const bot = this.client;
+        const bot = client;
         const player = bot.music.players.get(message.guild.id);
 
         if (!player) return message.channel.send("No song/s currently playing within this guild.");
