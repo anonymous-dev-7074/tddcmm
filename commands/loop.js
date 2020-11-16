e83957caa634f9826f1d8780e56b2612989fb29c
Raj Chatterjee <65060180+raj123567@users.noreply.github.com>
2020-11-16 15:05:47 +0530

    Create loop.js

---
+++ commands/loop.js
@@ -0,0 +1,38 @@
+module.exports = {
+help: {
+name: "loop"
+},
+run: async(client, message, args) => {
+const bot = client;
+
+            const player = bot.music.players.get(message.guild.id);
+    if(!args[0]) return message.channel.send("Please specify to enable or disable loop")
+    if(args[0] == 'queue' || args[0] == "queue"){
+    if(player.queueRepeat == false){
+    player.setQueueRepeat(true)
+            message.channel.send('queue loop has been turned on')
+    }
+            else if(player.queueRepeat == true){
+           player.setQueueRepeat(false)
+                    message.channel.send('queue loop has been turned off')
+            };
+    
+    
+    };
+     
+            if(args[0] == 'track' || args[0] == "t"){    
+    
+      if(player.trackRepeat == true){
+            player.setTrackRepeat(false)
+              message.channel.send({embed: {description: "trackloop has been turned off"})
+    
+      }
+      else if(player.trackRepeat == false){
+            player.setTrackRepeat(true)
+              message.channel.send("track loop has been turned on")
+    }
+                         
+    
+            }
+}
+}
