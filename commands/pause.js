b46a4c2114dbbe455ec4b6082699a106d7d8cbaf
Raj Chatterjee <65060180+raj123567@users.noreply.github.com>
2020-11-16 14:31:53 +0530

    Create pause.js

---
+++ commands/pause.js
@@ -0,0 +1,22 @@
+module.exports = {
+help: {
+name: "pause",
+aliases: ["resume"]
+},
+run: async(client, message, args) => {
+        const bot = this.client;
+    const player = bot.music.players.get(message.guild.id);
+
+    if (!player) return message.channel.send("No song/s currently playing in this guild.");
+
+    const voiceChannel = message.member.voice.channel;
+
+    if (!voiceChannel || voiceChannel.id !== player.voiceChannel.id) return message.channel.send("You need to be in a voice channel to pause music.");
+
+    
+
+    player.pause(player.playing);
+
+    return message.channel.send(`Player is now ${player.playing ? "resumed" : "paused"}.`);
+}
+}
