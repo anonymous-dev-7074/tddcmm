8772e59e9b5e6d6962294059f7431bf6eb139b6f
anonymous-dev-7074 <73049664+anonymous-dev-7074@users.noreply.github.com>
2020-11-17 11:51:33 +0530

    Create stats.js

---
+++ commands/stats.js
@@ -0,0 +1,51 @@
+{ MessageEmbed }= require('discord.js');
+const moment = require('moment');
+const { mem, cpu, os } = require('node-os-utils');
+const { stripIndent } = require('common-tags');
+
+module.exports = {
+    help: {
+        name: "stats",
+        aliases: ['bot-info'],
+        category: "info",
+        description: "Shows Bot Statistics",
+        usage: "",
+   },
+    run: async (client, message, args) => {
+        
+    const d = moment.duration(message.client.uptime);
+    const days = (d.days() == 1) ? `${d.days()} day` : `${d.days()} days`;
+    const hours = (d.hours() == 1) ? `${d.hours()} hour` : `${d.hours()} hours`;
+    const clientStats = stripIndent`
+      Servers   :: ${client.guilds.cache.size}
+      Users     :: ${client.users.cache.size}
+      Channels  :: ${client.channels.cache.size}
+      WS Ping   :: ${Math.round(client.ws.ping)}ms
+      Uptime    :: ${days} and ${hours}
+    `;
+    const { totalMemMb, usedMemMb } = await mem.info();
+    const serverStats = stripIndent`
+      OS        :: ${await os.oos()}
+      CPU       :: ${cpu.model()}
+      Cores     :: ${cpu.count()}
+      CPU Usage :: ${await cpu.usage()} %
+      RAM       :: ${totalMemMb} MB
+      RAM Usage :: ${usedMemMb} MB 
+    `;
+    
+    const embed = new MessageEmbed()
+      .setTitle('Bot\'s Statistics')
+      .addField('Commands', `\`${message.client.commands.size}\` commands`, true)
+      .addField('Aliases', `\`${message.client.aliases.size}\` aliases`, true)
+      .addField('Client', `\`\`\`asciidoc\n${clientStats}\`\`\``)
+      .addField('Server', `\`\`\`asciidoc\n${serverStats}\`\`\``)
+      .addField(
+        'Links', 
+        '**[Invite Me](https://discordapp.com/oauth2/authorize?client_id=768517226654859295&scope=bot&permissions=8)**'
+      )
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(message.guild.me.displayHexColor);
+    message.channel.send(embed);
+    }
+}
