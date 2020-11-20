2a4c9fb8d8222d5bdbc8f604e0a2d905839501b6
anonymous-dev-7074 <73049664+anonymous-dev-7074@users.noreply.github.com>
2020-11-20 10:29:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,44 @@ console.log(`Loaded ${files.length} events!`)
   });
 });
 
+client.on("message", async message => {
+   
+let argsresult = args.join(" ");
+    if (argsresult) {
+      let matches = argsresult.match(/:([a-zA-Z0-9-_~]+):/g);
+      if (matches)
+        for (const match of matches) {
+          const rep = await client.emojis.cache.find(
+            emoji => emoji.name === match.substring(2, match.length - 1)
+          );
+          if (rep) argsresult = argsresult.replace(match, rep);
+        }
+    }
+    // what u want to fetch with the avatar?
+    message.channel
+      .fetchWebhooks() // This gets the webhooks in the channel
+      .then(webhook => {
+        let foundHook = webhook.find(wb => wb.name === client.user.username);
+        if (!foundHook) {
+          message.channel
+            .createWebhook(client.user.username, client.user.displayAvatarURL())
+            .then(web => {
+              web.send(argsresult, {
+                username: message.author.username,
+                avatarURL: message.author.displayAvatarURL()
+              });
+            });
+        } else {
+          foundHook.send(argsresult, {
+            username: message.author.username,
+            avatarURL: message.author.displayAvatarURL()
+          });
+        }
+      });
+    message.delete();
+});
+
+
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 
