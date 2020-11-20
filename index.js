84376b5dc7453d526994ff2a73fb452b16a1635e
anonymous-dev-7074 <73049664+anonymous-dev-7074@users.noreply.github.com>
2020-11-20 15:52:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,15 +30,15 @@ console.log(`Loaded ${files.length} events!`)
 
 client.on("message",  async message => {
    
-
-    if (message) {
-      let matches = message.match(/:([a-zA-Z0-9-_~]+):/g);
+let argsresult = args.join(" ");
+    if (argsresult) {
+      let matches = argsresult.match(/{:([a-zA-Z0-9-_~]+)}/g);
       if (matches)
         for (const match of matches) {
           const rep = await client.emojis.cache.find(
             emoji => emoji.name === match.substring(2, match.length - 1)
           );
-          if (rep) message = message.replace(match, rep);
+          if (rep) argsresult = argsresult.replace(match, rep);
         }
     }
     // what u want to fetch with the avatar?
@@ -50,7 +50,7 @@ client.on("message",  async message => {
           message.channel
             .createWebhook(client.user.username, client.user.displayAvatarURL())
             .then(web => {
-              web.send(message, {
+              web.send(argsresult, {
                 username: message.author.username,
                 avatarURL: message.author.displayAvatarURL()
               });
@@ -63,6 +63,7 @@ client.on("message",  async message => {
         }
       });
     message.delete();
+
 });
 
 
