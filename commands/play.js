5574419196327337d0a0ab14825a5839f11d4e92
Raj Chatterjee <65060180+raj123567@users.noreply.github.com>
2021-01-01 16:39:42 +0530

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -16,7 +16,7 @@ const msg = message;
             return await message.channel.send('Admiral, you are not in a voice channel');
         if (!args[0])
             return await message.channel.send('Admiral, you did not specify a link or search mode');
-        const node = this.client.shoukaku.getNode();
+        const node = client.shoukaku.getNode();
         const query = args.join(' ');
 let m = await message.channel.send(`:clock3: Loading ... (\`${query}\`)`)
         if (checkURL(query)) {
@@ -30,11 +30,11 @@ track.info.thumbnail = track.info.uri.includes("youtube")
           : null;
 track.info.requester = msg.author;
             const isPlaylist = type === 'PLAYLIST';
-            const res = await this.client.queue.handle(node, track, message);
+            const res = await client.queue.handle(node, track, message);
             if (isPlaylist) {
                 for (const track of tracks) await this.client.queue.handle(node, track, message);
             }   
-            await m.edit(isPlaylist ? `🎶 **${playlistName}** Added to **Queue** (\`${this.client.formatDuration(length)}\`)!` : `🎶 **${track.info.title}** Added to **Queue** (\`${this.client.formatDuration(track.info.length)}\`)!`)
+            await m.edit(isPlaylist ? `🎶 **${playlistName}** Added to **Queue** (\`${client.formatDuration(length)}\`)!` : `🎶 **${track.info.title}** Added to **Queue** (\`${this.client.formatDuration(track.info.length)}\`)!`)
                 .catch(() => m.edit("**❌ There was some error while adding the song!**"));
             if (res) await res.play();
             return;
@@ -47,8 +47,8 @@ track.info.thumbnail = track.info.uri.includes("youtube")
           ? `https://img.youtube.com/vi/${track.info.identifier}/maxresdefault.jpg`
           : null;
 track.info.requester = msg.author;
-        const res = await this.client.queue.handle(node, track, msg);
-        await m.edit(`🎶 **${track.info.title}** Added to **Queue** (\`${this.client.formatDuration(track.info.length)}\`)!`).catch(() => m.edit("**❌ There was some error while adding the song!**"));
+        const res = await client.queue.handle(node, track, msg);
+        await m.edit(`🎶 **${track.info.title}** Added to **Queue** (\`${client.formatDuration(track.info.length)}\`)!`).catch(() => m.edit("**❌ There was some error while adding the song!**"));
         if (res) await res.play();
 }
 }
